ace888b80044376d7243541660f26c5478fbe55d
ywcz <ywczxj@qq.com>
2022-02-24 17:21:27 +0800

    下班下班

--- init.el
+++ init.el
@@ -1,3 +1,9 @@
+;; 快速打开配置文件
+(defun open-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+;; 这一行代码，将函数 open-init-file 绑定到 <f2> 键上
+(global-set-key (kbd "<f2>") 'open-init-file)
 
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -9,6 +15,8 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+(setq use-package-always-ensure t)
+
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/use-package.org"))
 
